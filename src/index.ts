fba8972d52b2d56b464a076cb57457f434241104
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-04-03 10:04:18 +0200

    Fix sending binary data

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { parse } from 'url';
 import LRU from 'lru-cache';
 import accesslog from 'access-log';
 import micri from 'micri';
+import { IncomingMessage, ServerResponse, send } from 'micri';
 import ms from 'ms';
-import { IncomingMessage, ServerResponse } from 'micri';
 import _apiFetch from './fetch-graph-api';
 import fetch from './fetch';
 import getEnv from './get-env';
@@ -47,15 +47,38 @@ function buildUrl(host: string, path: string): string {
 	return `${ROOT}${path}:`;
 }
 
-async function sendFile(res: ServerResponse, file: File) {
-	const data = await fetch(file['@microsoft.graph.downloadUrl']);
+async function sendFile(req: IncomingMessage, res: ServerResponse, file: File) {
+	const headers: { [key: string]: string | string[] } = {};
 
-	res.setHeader('Content-Type', file.file.mimeType || 'application/octet-stream');
+	if (req.headers['accept-encoding']) {
+		headers['accept-encoding'] = req.headers['accept-encoding'];
+	}
+
+	const data = await fetch(file['@microsoft.graph.downloadUrl'], {
+		compress: false,
+		// @ts-ignore
+		headers
+	});
+	const transferEncoding = data.headers.get('transfer-encoding');
+	const contentType = data.headers.get('content-type');
+	const contentEncoding = data.headers.get('content-encoding');
+	const len = data.headers.get('content-length');
+
+	if (transferEncoding) {
+		res.setHeader('transfer-encoding', transferEncoding);
+	}
+	res.setHeader('Content-Type', contentType || 'application/octet-stream');
+	if (contentEncoding) {
+		res.setHeader('Content-Encoding', contentEncoding);
+	}
+	if (len) {
+		res.setHeader('Content-Length', len);
+	}
 	res.setHeader('Content-Disposition', `inline; filename="${file.name}"`);
 	res.setHeader('Cache-Control', 'public, max-age=0, must-revalidate');
 	res.setHeader('ETag', file.eTag);
 
-	return data.text();
+	send(res, data.status, data.body);
 }
 
 const server = micri(async (req: IncomingMessage, res: ServerResponse) => {
@@ -74,8 +97,6 @@ const server = micri(async (req: IncomingMessage, res: ServerResponse) => {
 	const graphUrl = buildUrl(host, url.pathname || '');
 	const meta = await apiFetch(graphUrl);
 
-	console.log(graphUrl);
-
 	if (!meta) {
 		return sendError(req, res, 404, {
 			code: 'not_found',
@@ -91,7 +112,7 @@ const server = micri(async (req: IncomingMessage, res: ServerResponse) => {
 				o.name.toLowerCase().startsWith('index.')
 		);
 		if (index) {
-			return sendFile(res, index);
+			return sendFile(req, res, index);
 		} else {
 			// TODO File listing
 			return sendError(req, res, 404, {
@@ -100,7 +121,7 @@ const server = micri(async (req: IncomingMessage, res: ServerResponse) => {
 			});
 		}
 	} else if (meta.file) {
-		return sendFile(res, meta);
+		return sendFile(req, res, meta);
 	}
 
 	return sendError(req, res, 404, {
