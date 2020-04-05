588ba8f88270b8b58914b242d0ce49b53151503a
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-04-06 00:23:55 +0200

    More proper Cache-Control

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const CACHE_SEC = 30;
-export const CACHE_CONTROL = `public, max-age=${CACHE_SEC}, must-revalidate, s-maxage=${CACHE_SEC}, stale-while-revalidate`;
+export const CACHE_CONTROL = `public, no-cache, max-age=0, stale-while-revalidate=${CACHE_SEC}, s-maxage=${CACHE_SEC}, no-transform`;
 export const DISABLE_FILE_LISTING = false;
 export const PROTECTED_FILES = [/\.swp$/];
 export const HIDDEN_FILES = [/^\./, ...PROTECTED_FILES];
