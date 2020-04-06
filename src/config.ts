1951437b5009ba539fa3ef81d410d523f65ea5a1
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-04-07 01:35:34 +0200

    Disable functions by default

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ export const ENABLE_FILE_LISTING = false;
 export const PROTECTED_FILES = [/\.swp$/];
 export const HIDDEN_FILES = [/^\./, ...PROTECTED_FILES];
 export const INDEX_PATTERN = /^index\..*/i;
-export const ENABLE_FUNCTIONS = true;
+export const ENABLE_FUNCTIONS = false;
 export const FUNCTION_PATTERN = /.*\.fjs$/;
