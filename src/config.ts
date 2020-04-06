e02d511cffd14dd313994daca202f50688ea14fa
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-04-06 09:36:24 +0200

    Preliminary support for functions
    - Match executable functions by `.fjs` suffix.
    - Run functions in worker threads

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
 export const CACHE_SEC = 30;
 export const CACHE_CONTROL = `public, no-cache, max-age=0, stale-while-revalidate=${CACHE_SEC}, s-maxage=${CACHE_SEC}, no-transform`;
-export const DISABLE_FILE_LISTING = false;
+export const ENABLE_FILE_LISTING = false;
 export const PROTECTED_FILES = [/\.swp$/];
 export const HIDDEN_FILES = [/^\./, ...PROTECTED_FILES];
+export const INDEX_PATTERN = /^index\..*/i;
+export const ENABLE_FUNCTIONS = true;
+export const FUNCTION_PATTERN = /.*\.fjs$/;
